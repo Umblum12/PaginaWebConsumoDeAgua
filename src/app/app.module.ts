2741f8c804e7d76384ba288ee2b47ba32fbaeb9a
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2023-04-03 07:30:26 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Modulos
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+//Componentes
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { VerificarCorreoComponent } from './components/verificar-correo/verificar-correo.component';
+import { RecuperarPasswordComponent } from './components/recuperar-password/recuperar-password.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrarUsuarioComponent,
+    DashboardComponent,
+    VerificarCorreoComponent,
+    RecuperarPasswordComponent,
+    SpinnerComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyD8d76S07BjkoyJFErIqojRGQ5gGJUQeI4",
+      authDomain: "consumodeaguapi.firebaseapp.com",
+      databaseURL: "https://consumodeaguapi-default-rtdb.firebaseio.com",
+      projectId: "consumodeaguapi",
+      storageBucket: "consumodeaguapi.appspot.com",
+      messagingSenderId: "702040109967",
+      appId: "1:702040109967:web:277709d5f42f01286651fa"
+    }
+  };
